19e7164e87c6534b99ee92d9f6e63617b9d81c40
Ryan Reynolds <ryanreynolds01@gmail.com>
2016-03-12 17:11:10 +0000

    Added stripe js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
+    // prevent default form submission so that the rest of the code can be executed
+    event.preventDefault();
+    // disables button so people can't click on it two or three times after its been clicked already
+    $('input[type=submit]').prop('disabled', true);
+    var error = false;
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+
+    if (!error) {
+      // Get the Stripe token:
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
+  });// form submission
+  
+  function stripeResponseHandler(status, response) {
+    // Get a reference to the form:
+    var f = $("#new_user");
+
+    // Get the token from the response:
+    var token = response.id;
+
+    // Add the token to the form:
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+
+    // Submit the form. get(0) means to select the first form in the case that multiple forms are on the page
+    f.get(0).submit(); 
+  }
+});
