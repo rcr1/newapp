8c08077332ed57478f3b8408f7415c1bc23e79dc
Ryan Reynolds <ryanreynolds01@gmail.com>
2016-03-15 03:57:59 +0000

    Modified devise for stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  
+  #Devises way of whitelisting parameters that we add to the devise users table
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:signup) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmaton) }
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  # Allows us to access stripe card token used in the javascript in users.js
+  attr_accessor :stripe_card_token
+  
+  def save_with_payment
+    # tests any validations that are used
+    if valid?
+      # gets stripe gem to send out information to stripe, creating customer and charging customer
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      # stripe returns an id to us for the particular customer, which is then saved
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
 end
