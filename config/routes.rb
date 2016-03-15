50bde318c7effdfe25316a576ee1438fb61bca54
Ryan Reynolds <ryanreynolds01@gmail.com>
2016-03-15 04:03:30 +0000

    Updated routes file with new devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :contacts
   root 'pages#home'
   get '/about' => 'pages#about'
+  
 
   
   # The priority is based upon order of creation: first created -> highest priority.
