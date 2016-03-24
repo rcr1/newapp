689b0234d304a5f3bfe454c4d822a0bc20612c18
Ryan Reynolds <ryanreynolds01@gmail.com>
2016-03-24 04:08:07 +0000

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,9 @@
 class ProfilesController < ApplicationController
-  def create
-    
+  def new
+      # form where a user can fill out their own profile
+      @user = User.find( params[:user_id] )
+      # rails gives us build_profile method because we established the model associations between the users and profiles (in routes file, profiles is nested under users)
+      @profile = @user.build_profile
   end
-  
-  def edit
-  
-  end
+
 end
