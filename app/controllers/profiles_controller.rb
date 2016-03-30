322c5c739d8a0735b1aded37ba227a2e5e739b8e
Ryan Reynolds <ryanreynolds01@gmail.com>
2016-03-30 04:02:57 +0000

    Changed new action in prorfiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
       # form where a user can fill out their own profile
       @user = User.find( params[:user_id] )
       # rails gives us build_profile method because we established the model associations between the users and profiles (in routes file, profiles is nested under users)
-      @profile = @user.build_profile
+      @profile = Profile.new
   end
   
   def create
