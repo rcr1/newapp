30ac427582655214f79a8699798e1e697b083603
Ryan Reynolds <ryanreynolds01@gmail.com>
2016-04-04 00:54:26 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < ApplicationController
+  # has Devise authenticate all user actions before any of the following methods in this controller file
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
   def new
       # form where a user can fill out their own profile
       @user = User.find( params[:user_id] )
@@ -39,5 +43,10 @@ class ProfilesController < ApplicationController
     # :profile tells Rails which object can be saved to the database 
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
     def show
-       @user = User.find( params[:id] ) 
+      if current_user
+        @user = User.find( params[:id] ) 
+      else
+        redirect_to root_path
+        flash[:danger] = "You must be signed in to view user profiles."
+      end
     end
     
-    def index
-       @users = User.all 
-    end
+  def index
+    @users = User.all 
+  end
+  
 end
